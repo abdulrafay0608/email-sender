2a9b7242600b147319b34cbc820071143004a5b0
Abdul Rafay <121513581+abdulrafay0608@users.noreply.github.com>
2025-03-01 03:27:02 +0500

    Update emailsender.controllers.js

--- controllers/emailsender.controllers.js
+++ controllers/emailsender.controllers.js
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import generateEmailHtml from "../helpers/emailtemplategenerate.helper.js";
+import { generateEmailHtml } from "../helpers/emailtemplategenerate.helper.js";
 
 export const EmialSenderController = async (req, res) => {
   const { name, email, subject, message } = req.body;
