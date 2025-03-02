3be57f894b3f4a23f348131d6fc9672bd13e764f
Abdul Rafay <121513581+abdulrafay0608@users.noreply.github.com>
2025-03-03 02:13:27 +0500

    add cors origin

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (_, res) => {
   res.send("Welcome to the Abdul Rafay Portfolio Backend!");
